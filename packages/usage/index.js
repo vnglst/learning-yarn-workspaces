665a9880f641da35a49aa96497af13026814df5d
Koen van Gilst <koen.vangilst@gmail.com>
2018-11-06 19:55:17 +0100

    cleanup

--- packages/usage/index.js
+++ packages/usage/index.js
@@ -1,4 +1,3 @@
-var alpha = require('vnglst-alpha')
 var gamma = require('vnglst-gamma')
 var delta = require('vnglst-delta')
-console.log(alpha + ' ' + gamma + ' ' + delta)
+console.log(gamma + ' ' + delta)
